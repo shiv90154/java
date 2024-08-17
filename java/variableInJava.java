fc4d3a827eca28ad98a66bbfe86bbba6b1668027
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-08-17 15:30:21 +0530

    Add files via upload
    Pattern1 in java and variable
     in java

---
+++ java/variableInJava.java
@@ -0,0 +1,16 @@
+public class variableInJava {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        int c = 15;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        a = 20;
+        System.out.println(a);
+        a = b;
+        System.out.println(a);
+    }
+
+}
