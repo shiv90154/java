570f225124ee8510efd2605610e0c4928a9e1867
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-09-15 21:37:34 +0530

    Add files via upload

---
+++ java/inputInjava.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class inputInjava {
+    public static void main(String[] args) {
+
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+        int number = sc.nextInt();
+        System.out.println(number);
+
+    }
+
+}
