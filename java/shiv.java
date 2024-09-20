9b33d5226f89f089d01f75e904a675fe883dff15
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-09-21 01:57:09 +0530

    Add files via upload

---
+++ java/shiv.java
@@ -0,0 +1,18 @@
+
+public class shiv {
+
+    public static void main(String[] args) {
+        int number = 1;
+        switch (number) {
+            case 1:
+                System.out.println("smosa kho");
+                break;
+            case 2:
+                System.out.println("tum kho");
+                break;
+            default:
+                System.out.println("shiv");
+
+        }
+    }
+}
