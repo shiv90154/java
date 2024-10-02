0ea6b5add0d5db306d701d5fdad2641a2ca59475
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-10-02 22:54:34 +0530

    Add files via upload

---
+++ Array/Afirst.java
@@ -0,0 +1,13 @@
+
+import java.util.Arrays;
+
+public class Afirst {
+    // how to print an value in java
+    public static void main(String[] args) {
+        String[] name = { "shiv", "nikhil", "dogra", "honey" };
+        int[] num = { 1, 2, 3, 4, 5, 6, 7 };
+        System.out.print(Arrays.toString(name));
+        System.err.println("\n");
+        System.out.print(Arrays.toString(num));
+    }
+}
