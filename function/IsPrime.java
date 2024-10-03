38024d4ca40e4ee750ebc501cb4ab1db5fb75cd4
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-10-03 16:44:23 +0530

    Add files via upload

--- function/IsPrime.java
+++ function/IsPrime.java
@@ -1,16 +1,31 @@
 public class IsPrime {
 
+    // public static boolean IsPrime(int n) {
+    // boolean IsPrime = true;
+    // for (int i = 2; i <= n - 1; i++) {
+
+    // if (n % i == 0) {
+    // IsPrime = false;
+    // break;
+    // }
+    // }
+    // return IsPrime;
+
+    // }
+
+    // OPtimized code for prime number
+
     public static boolean IsPrime(int n) {
-        boolean IsPrime = true;
-        for (int i = 2; i <= n - 1; i++) {
-
-            if (n % i == 0) {
-                IsPrime = false;
-                break;
-            }
+        if (n == 2) {
+            return true;
         }
-        return IsPrime;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
 
+        }
+        return true;
     }
 
     public static void main(String[] args) {
